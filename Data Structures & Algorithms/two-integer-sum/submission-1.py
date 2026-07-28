b568efaebe9ef4a1b95adebdb3239ccc40883a8d
Alwin Mathew <alwinmathew1107@gmail.com>
2026-07-28 10:09:57 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      #Brute force - O(n^2) Time Comp
+      for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
+      return []  #If solution doesn't exist
+
+        
