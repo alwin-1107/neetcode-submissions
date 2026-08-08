50eff2e3634e0f15ba30aa64ef8c642f2a60f350
Alwin Mathew <alwinmathew1107@gmail.com>
2026-08-09 04:45:11 +0530

    Add: sql-customers-bought-a-b-not-c - submission-3

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-3.sql
@@ -0,0 +1,8 @@
+SELECT c.customer_id,c.customer_name FROM customers c
+WHERE 
+c.customer_id IN(SELECT o.customer_id FROM orders o WHERE product_name = 'A')
+
+AND c.customer_id IN(SELECT o.customer_id FROM orders o WHERE product_name = 'B')
+
+AND c.customer_id NOT IN(SELECT o.customer_id FROM orders o WHERE product_name = 'C')
+ORDER BY c.customer_name;
